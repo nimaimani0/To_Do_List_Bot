8bfee2f5578781d61ccda2e9300f8c38906d578b
Mehdi <mahdiahmadiyan13@gmail.com>
2026-08-31 13:35:26 +0330

    Create a project structure for database CRUD operations

---
+++ src/database.py
@@ -0,0 +1,17 @@
+def init():
+    pass
+
+def add(user_id, task_text):
+    pass
+
+def get(user_id):
+    pass
+
+def edit_task_status():
+    pass
+
+def edit_task_text():
+    pass
+
+def delete(task_id):
+    pass
