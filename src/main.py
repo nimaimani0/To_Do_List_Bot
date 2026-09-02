a80365a4932513a403c6334773d0f5d12820d468
Mehdi <mahdiahmadiyan13@gmail.com>
2026-09-02 14:42:39 +0330

    Fixed a security issue in src/main.py file

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher
+from dotenv import load_dotenv
 
 import database
 import handlers
 
-BOT_TOKEN = "8903844663:AAEVDAZ_jEl3HT9eAdCrSzWtAYnRUyDfxl0"
+
+load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def main():
     database.init()
